ef7d94d8a2de9de44e926b8a49c573950dd17eb0
Gaurav Singh <gauravsinghigc@gmail.com>
2024-11-17 15:11:57 +0530

    Initial commit

---
+++ index.php
@@ -0,0 +1,141 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+ <meta charset="UTF-8">
+ <meta name="viewport" content="width=device-width, initial-scale=1.0">
+ <meta http-equiv="X-UA-Compatible" content="ie=edge">
+ <meta name="Description" content="Gaurav Weds Deepa on 17 Feb 2022." />
+ <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.1.0/css/bootstrap.min.css">
+ <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+ <link rel="stylesheet" href="assets/css/style.css">
+ <title>Gaurav Weds Deepa</title>
+ <style>
+  .img0 {
+   position: fixed !important;
+   left: 0px;
+   height: 100% !important;
+   bottom: 5.5%;
+  }
+
+  body {
+   background-image: url('https://cliply.co/wp-content/uploads/2021/09/CLIPLY_372109170_FREE_FIREWORKS_400.gif');
+   background-position: center;
+   background-size: ;
+   background-color: black;
+  }
+
+  .img2 {
+   position: fixed !important;
+   right: 0px !important;
+   bottom: 0px !important;
+   width: 25% !important;
+  }
+
+  .img3 {
+   position: fixed !important;
+   left: 0px !important;
+   bottom: 0px !important;
+   width: 25% !important;
+   display: none;
+  }
+
+  .img4 {
+   position: fixed !important;
+   left: 40%;
+   top: 0px !important;
+   width: 35% !important;
+  }
+
+  @media only screen and (max-width: 600px) {
+   .img3 {
+    display: block;
+   }
+
+   .img2,
+   .img3 {
+    width: 100% !important;
+   }
+
+   .img4 {
+    left: 25%;
+    width: 50% !important;
+   }
+
+   .dholak1,
+   .dholak2 {
+    width: 25% !important;
+    bottom: 1px;
+   }
+
+   .doli {
+    width: 20% !important;
+   }
+  }
+
+  .dholak1 {
+   width: 10%;
+   position: fixed !important;
+   left: -10px;
+   bottom: -15px;
+   z-index: 2;
+  }
+
+  .dholak2 {
+   width: 10%;
+   position: fixed !important;
+   right: -10px;
+   bottom: -15px;
+   z-index: 2;
+  }
+
+  .doli-container {
+   position: fixed;
+   bottom: 0px;
+   z-index: 1;
+   left: 0px;
+   height: 6.5%;
+   background-color: #581137;
+  }
+
+  .doli {
+   width: 5%;
+   margin-bottom: 10px;
+   -webkit-transform: scaleX(-1);
+   transform: scaleX(-1);
+  }
+
+  marquee {
+   margin-top: -2%;
+  }
+ </style>
+</head>
+
+<body>
+ <section class="container-fluid">
+  <div class="row">
+   <div class="col-md-12 pl-0 pr-0">
+    <p>
+     <span>Dear,</span>
+    </p>
+    <img src="gd.jpg" class="img-fluid img0">
+    <img src="https://img1.picmix.com/output/stamp/normal/6/2/3/4/534326_39fd0.gif" class="img-fluid img2">
+    <img src="https://img1.picmix.com/output/stamp/normal/6/2/3/4/534326_39fd0.gif" class="img3 img-fluid">
+    <img src="https://img1.picmix.com/output/stamp/thumb/6/2/3/4/534326_39fd0.gif" class="img4 img-fluid">
+    <img src="https://i.imgur.com/ioUZwu4.gif" class="dholak1">
+    <img src="https://i.imgur.com/ioUZwu4.gif" class="dholak2">
+    <div class="doli-container">
+     <marquee direction="right" class="doli-container" behavior="scroll">
+      <img src="https://i.pinimg.com/originals/48/1d/64/481d64cc333a998a922e6a905e5de364.gif" class="doli img-fluid">
+     </marquee>
+    </div>
+   </div>
+  </div>
+ </section>
+ <audio hidden="" controls autoplay>
+  <source src="dhol.ogg" type="audio/ogg">
+  <source src="dhol.mp3" type="audio/mpeg">
+ </audio>
+</body>
+
+</html>
